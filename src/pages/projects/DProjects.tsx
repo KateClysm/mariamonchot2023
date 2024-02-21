d0215348b00c3e7dba3ade00cf1110b7ca60ddb3
Maria Monchot <mariamonchot@gmail.com>
2024-02-21 19:46:16 -0300

    VibrAr project added

--- src/pages/projects/DProjects.tsx
+++ src/pages/projects/DProjects.tsx
@@ -8,6 +8,35 @@ import IProject from "../../interfaces/IProject";
 const DProjects: IProject[] = [
   {
     id: 1,
+    title: "VibrAr",
+    card_description: "Informative Music Concerts Page",
+    description: "VibrAr is your essential guide to the latest events and rumors about concerts by artists coming to Argentina. Explore the exciting music scene with detailed information about dates, venues, and fresh news. Immerse yourself in the VibrAr experience, where music comes to life. Discover, vibe, and stay updated with the magic of music in Argentina!",
+    key_features: [
+      "Concert Rumors: Stay informed about potential concerts happening in Argentina, exploring the latest rumors and speculations.",
+    "Confirmed Concerts: Discover which concerts are officially confirmed, along with relevant information about each event.",
+    "Travel Tips: Obtain valuable advice for travel, accommodation, and enhancing your overall concert experience."],
+    my_rol: "In this project, I had the opportunity to refine my front-end skills and create a vibrant, captivating, and responsive website. Using technologies such as React.js, JavaScript, Sass, and Framer Motion, I contributed to crafting an engaging user interface. Additionally, I collaborated with Agustina Barbona in designing the distinctive logo for the website, enhancing its visual appeal and brand identity.",
+    photo: "vibrar/principal.png",
+    mobile_view: "vibrar/mobile.png",
+    secondary_view: "vibrar/secondary.png",
+    technologies: [
+      { icon: <GrReactjs />, name: 'React.js' },
+      { icon: <IoLogoJavascript />, name: 'JavaScript' },
+      { icon: <FaHtml5 />, name: 'HTML5' },
+      { icon: <FaCss3Alt />, name: 'CSS3' },
+      { icon: <FaSass />, name: 'Sass' },
+      { icon: <FaNodeJs />, name: 'Node.js' },
+      { icon: <FaGithubSquare />, name: 'GitHub' },
+      { icon: <FaNpm />, name: 'npm' },
+      { icon: <FaFigma />, name: 'Figma' },
+
+    ],
+    git: "https://github.com/KateClysm/VibrAr2.0",
+    link: "",
+    date: "February 2023",
+  },
+  {
+    id: 2,
     title: "ParaSeekers",
     card_description: "Paranormal Forum",
     description: "ParaSeekers is an interactive forum devoted to exploring the paranormal world. Users can create personalized profiles, share their mysterious encounters, and delve into supernatural experiences. This community-driven space fosters connections among individuals intrigued by the unexplained, providing a supportive environment to contribute unique ghostly stories and explore inexplicable phenomena.",
@@ -41,7 +70,7 @@ const DProjects: IProject[] = [
   },
 
   {
-    id: 2,
+    id: 3,
     title: "Sakura Calculator",
     card_description: "Simple Calculator Platform",
     description: "The 'Sakura Calculator' is a visually enchanting platform that offers standard arithmetic functions within a Sakura-inspired design.",
