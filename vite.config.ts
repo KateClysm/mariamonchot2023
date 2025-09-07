9f9145af3cbf06fbea98cdaec8bf1b353d2850a0
Maria Monchot <mariamonchot@gmail.com>
2025-09-07 13:55:17 -0300

    Cambio de nombre de Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://kateclysm.github.io/mariamonchot/'
+  base:'/mariamonchot2023/'
 })
